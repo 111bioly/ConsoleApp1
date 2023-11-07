0cd5de5cdaabdf917910a29fdbf4caa03c7be8cf
Tomasz Szabla <111thomas1111@gmail.com>
2023-11-07 19:19:55 +0100

    Zadanie 4

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,17 @@
+﻿var myName = "Ewa";
+string mySex = "kobieta";
+string mySexM = "mężczyzna";
+var myAge = 33;
+var myAge2 = 17;
+if (myName == "Ewa" && mySex == "kobieta" && myAge == 33)
+{
+    Console.WriteLine("Mam na imie Ewa mam 33 lat");
+}
+else if (mySex == "kobieta" && myAge < 30)
+{
+    Console.WriteLine("jestm kobietą poniżej 30 lat");
+}
+else if (mySexM == "mężczyzna" && myAge2 < 18)
+{
+    Console.WriteLine("jestem niepełnoletni");
+}
